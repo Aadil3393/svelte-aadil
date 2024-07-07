0e0f5a62c1fe337720c263053509daec3d3f8482
Aadil3393 <aadil.mohan3@gmail.com>
2024-07-07 11:31:16 +0530

    Updated database name from names to NAMES

--- src/routes/blog/[slug]/+page.server.js
+++ src/routes/blog/[slug]/+page.server.js
@@ -2,6 +2,6 @@ import { sql } from "@vercel/postgres";
 
 export async function load({ params, locals }) {
   return {
-    names: await sql`SELECT * from names`,
+    names: await sql`SELECT * from NAMES`,
   };
 }
